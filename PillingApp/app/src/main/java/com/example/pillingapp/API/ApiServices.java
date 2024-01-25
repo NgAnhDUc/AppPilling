597a3821f89b9862cf433425de90f125121d3362
NgAnhDUc <86831818+NgAnhDUc@users.noreply.github.com>
2024-01-25 10:25:43 +0700

    comit

--- PillingApp/app/src/main/java/com/example/pillingapp/API/ApiServices.java
+++ PillingApp/app/src/main/java/com/example/pillingapp/API/ApiServices.java
@@ -8,23 +8,24 @@ import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
-import retrofit2.http.Url;
+import retrofit2.http.Query;
 
-public class ApiService {
+public interface ApiServices {
+
+    //https://localhost:44321/api/Account/login?id=0&pass=string
     String BaseURL ="https://localhost:44321/";
     Gson gson = new GsonBuilder()
             .setDateFormat("yyyy-MM-dd HH:mm:ss")
             .create();
 
-    ApiService apiService = new Retrofit.Builder()
+    ApiServices apiService = new Retrofit.Builder()
             .baseUrl(BaseURL)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
-            .create(ApiService.class);
-    //
-    @GET("api/Account")
-    Call<User> login() {
-        return null;
-    }
-}
+            .create(ApiServices.class);
+
+    @GET("api/Account/login")
+    Call<User> login(@Query("id") int id,
+                     @Query("pass") String pass );
+
 }
