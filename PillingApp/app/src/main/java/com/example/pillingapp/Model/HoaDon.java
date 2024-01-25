2b6d2527d754459b03693fb98d4012d2f61a7c44
NgAnhDUc <86831818+NgAnhDUc@users.noreply.github.com>
2024-01-25 23:17:43 +0700

    frontend

---
+++ PillingApp/app/src/main/java/com/example/pillingapp/Model/HoaDon.java
@@ -0,0 +1,50 @@
+package com.example.pillingapp.Model;
+
+public class HoaDon {
+    String date;
+    int chiSoDien;
+    int chiSoNuoc;
+    int donGiaDien;
+    int donGiaNuoc;
+    int tongTien;
+
+    public int getChiSoDien() {
+        return chiSoDien;
+    }
+
+    public void setChiSoDien(int chiSoDien) {
+        this.chiSoDien = chiSoDien;
+    }
+
+    public int getChiSoNuoc() {
+        return chiSoNuoc;
+    }
+
+    public void setChiSoNuoc(int chiSoNuoc) {
+        this.chiSoNuoc = chiSoNuoc;
+    }
+
+    public int getDonGiaDien() {
+        return donGiaDien;
+    }
+
+    public void setDonGiaDien(int donGiaDien) {
+        this.donGiaDien = donGiaDien;
+    }
+
+    public int getDonGiaNuoc() {
+        return donGiaNuoc;
+    }
+
+    public void setDonGiaNuoc(int donGiaNuoc) {
+        this.donGiaNuoc = donGiaNuoc;
+    }
+
+    public int getTongTien() {
+        return tongTien;
+    }
+
+    public void setTongTien(int tongTien) {
+        this.tongTien = tongTien;
+    }
+}
